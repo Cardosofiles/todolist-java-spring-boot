81984d48deba51514c360af951db0a20e127d309
cardosofiles <cardosofiles@outlook.com>
2025-01-29 10:42:59 -0300

    chore: user route getters and setters

---
+++ src/main/java/br/com/minimalist/todolist/user/UserController.java
@@ -0,0 +1,26 @@
+package br.com.minimalist.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/users")
+
+public class UserController {
+  @PostMapping("/")
+  public void create(@RequestBody UserModel userModel) {
+    System.out.println(userModel.getUsername());
+  }
+}
+/**
+ * String - text
+ * Integer - number whole
+ * Double - numbers with decimal places
+ * Float - numbers with decimal places
+ * Char - characters special
+ * Date - work with dates
+ * void - 
+ */
